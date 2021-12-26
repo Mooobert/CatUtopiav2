92f435e5b5c82653d3542dfb5dea73d3cb391a5c
Mooobster <misha@burnayev.com>
2021-12-25 21:28:10 -0500

    level field list creation

--- facility_generation.dart
+++ facility_generation.dart
@@ -7,11 +7,24 @@ class FacilityAddition {
     return facilityType;
   }
 
-  List<List<String>> populateAllFacilities(int cookCount, int carrotCount,
-      int cabbageCount, int cornCount, int acornCount, int broccoliCount,
-      int lemonCount, int honeyCount, int wheatCount, int radishCount,
-      int pumpkinCount, int mushroomCount, int celeryCount, int restCount,
-      int fxnCount, int specCount, int emptyCount) {
+  List<List<String>> populateAllFacilities(
+      int cookCount,
+      int carrotCount,
+      int cabbageCount,
+      int cornCount,
+      int acornCount,
+      int broccoliCount,
+      int lemonCount,
+      int honeyCount,
+      int wheatCount,
+      int radishCount,
+      int pumpkinCount,
+      int mushroomCount,
+      int celeryCount,
+      int restCount,
+      int fxnCount,
+      int specCount,
+      int emptyCount) {
     List<List<String>> facilityList = <List<String>>[];
 
     facilityList.add(populateFacilityType("cooking", cookCount));
@@ -33,5 +46,67 @@ class FacilityAddition {
     facilityList.add(populateFacilityType("(empty)", emptyCount));
 
     return facilityList;
+  }
+}
+
+class LevelLayoutGeneration {
+  List<List<String>> generateRows(int level){
+    List<List<String>> generatedField;
+    if (level < 0) throw Exception("Can't generate field");
+
+    switch (level){
+      case 0:
+        generatedField = List.generate(5, (_) => []);
+        for(int i = 0; i < generatedField[i].length; i++){
+          generatedField[i] = i % 2 == 0 ? List<String>.filled(1, "") : List<String>.filled(2, "");
+        }
+        break;
+      case 1:
+        generatedField = List.generate(7, (_) => []);
+        for(int i = 0; i < generatedField[i].length; i++){
+          generatedField[i] = i % 2 == 0 ? List<String>.filled(2, "") : List<String>.filled(3, "");
+        }
+        break;
+      case 2:
+        generatedField = List.generate(9, (_) => []);
+        for(int i = 0; i < generatedField[i].length; i++){
+          generatedField[i] = i % 2 == 0 ? List<String>.filled(3, "") : List<String>.filled(4, "");
+        }
+        break;
+      case 3:
+        generatedField = List.generate(13, (_) => []);
+        for(int i = 0; i < generatedField[i].length; i++){
+          if (i % 2 == 1) {
+            generatedField[i] = List<String>.filled(4, "");
+          }
+          else if(i % 2 == 0 && i < 3 || i > 9){
+            generatedField[i] = List<String>.filled(3, "");
+          }
+          else{
+            generatedField[i] = List<String>.filled(5, "");
+          }
+        }
+        break;
+      case 4:
+        generatedField = List.generate(17, (_) => []);
+        for(int i = 0; i < generatedField[i].length; i++){
+          if (i == 0 || i == generatedField.length-1){
+            generatedField[i] = List<String>.filled(1, "");
+          }
+          else if( i % 2 == 0){
+            generatedField[i] = List<String>.filled(5, "");
+          }
+          else if(i % 2 == 1 && i < 4 || i > 12){
+            generatedField[i] = List<String>.filled(4, "");
+          }
+          else{
+            generatedField[i] = List<String>.filled(6, "");
+          }
+        }
+        break;
+      default:
+        throw Exception("Can't generate field!");
+    }
+    return generatedField;
   }
 }
